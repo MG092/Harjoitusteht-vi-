f18fc2fcbae5300a8cf3e27063fbdab6c672fb48
”Mendih <mendih.gezer@hotmail.com>
2020-09-13 14:37:57 +0300

    .

--- Koodaushaaste 1/2303/js/koodi.js
+++ Koodaushaaste 1/2303/js/koodi.js
@@ -1,3 +1,24 @@
-function Tervehdys() {
-alert("Heippa Maailma")
+var Muuttuja = [2, 5, 9];
+
+function laskeYhteen(a, b) {
+
+console.log (a + b);
+}
+
+function ekaMuuttuja() {
+  console.log (Muuttuja[0]);
+}
+
+function suurempiKuin(a, b)
+{
+
+if ((a + b) < 100)
+{
+
+console.log("Lukujen summa on alle 100");
+}
+else {
+  console.log("Luvut ovat liian suuria");
+}
+
 }
