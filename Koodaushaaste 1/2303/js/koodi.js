9e2b61c63af9da0b877e016cef5717538fee3822
”Mendih <mendih.gezer@hotmail.com>
2020-09-13 15:42:22 +0300

    .

--- Koodaushaaste 1/2303/js/koodi.js
+++ Koodaushaaste 1/2303/js/koodi.js
@@ -20,5 +20,19 @@ console.log("Lukujen summa on alle 100");
 else {
   console.log("Luvut ovat liian suuria");
 }
+}
+function Samat(a, b) {
+  if (a == b) {
+      console.log("Samat luvut");
+  }
+  else {
+    console.log("Eri luvut");
+  }
+}
+function sekunneiksi(a, b, c) {
+
+console.log(a * 3600 + b * 60 + c);
+}
+function functionName() {
 
 }
